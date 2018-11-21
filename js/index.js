28a59db41eab6aa3529268b37d08411230e845f6
nawatend <32384185+nawatend@users.noreply.github.com>
2018-11-21 18:50:18 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,291 @@
+let menuNav = document.getElementById("nav_menu");
+let viewMenu = document.getElementsByClassName("view_menu")[0];
+let locationNav = document.getElementById("nav_location");
+let foodMenu = document.getElementsByClassName("food__menu")[0];
+
+menuNav.addEventListener("click", () => {
+  document.getElementById("section_menu").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+
+viewMenu.addEventListener("click", () => {
+  document.getElementById("section_menu").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+
+locationNav.addEventListener("click", () => {
+  document.getElementById("section_location").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+
+const formatter = new Intl.NumberFormat("it-IT", {
+  style: "currency",
+  currency: "EUR"
+});
+
+loadMenuSmall = () => {
+
+  let arrTS = menuItemsJSON["tibetan specialties"];
+  let arrAS = menuItemsJSON["Asian snacks"];
+  let arrD = menuItemsJSON["Drinks"];
+  let arrDessert = menuItemsJSON.Dessert;
+
+  let arrAll = [{
+    'Tibetan Specialties': arrTS
+  }, {
+    'Asian Snacks': arrAS
+  }, {
+    'Dessert': arrDessert
+  }];
+
+  arrAll.forEach(type => {
+
+    let divType = document.createElement('div');
+    divType.classList.add("food__menu--type");
+    divType.textContent = Object.keys(type)[0];
+
+    let itemHolderUl = document.createElement("ul");
+    itemHolderUl.classList.add("collapsible");
+
+    type[Object.keys(type)[0]].forEach(item => {
+      let itemHolderLi = document.createElement("li");
+      let divItem = document.createElement("div");
+
+      divItem.classList.add("food__menu--item");
+      divItem.classList.add("collapsible-header");
+
+      //append to divItem
+      let divName = document.createElement("div");
+      divName.classList.add("food__name");
+
+      divName.textContent = item.name;
+
+      let divPrice = document.createElement("div");
+      divPrice.classList.add("food__price");
+      divPrice.textContent = formatter.format(item.price);
+
+      let divContent = document.createElement("div");
+      divContent.classList.add("collapsible-body");
+
+      divContent.textContent =
+        "LKslda orem ipsum dolor sit amet orem ipsum dolor sit ametorem ipsum dolor sit amet ";
+
+      divItem.appendChild(divName);
+      divItem.appendChild(divPrice);
+
+      itemHolderLi.appendChild(divItem);
+      itemHolderLi.appendChild(divContent);
+      itemHolderUl.appendChild(itemHolderLi);
+    })
+    divType.appendChild(itemHolderUl);
+    foodMenu.appendChild(divType);
+  });
+
+  // DRINKS
+
+  let divType = document.createElement("div");
+  divType.classList.add("food__menu--type");
+  divType.textContent = "Drinks";
+
+
+
+  arrD.forEach(category => {
+    let divCateD = document.createElement("div");
+    divCateD.classList.add("food__menu--category");
+    divCateD.textContent = Object.keys(category)[0];
+
+    let itemHolderUl = document.createElement("ul");
+    itemHolderUl.classList.add("collapsible");
+
+    category[Object.keys(category)[0]].forEach(item => {
+      //append to divType
+      let itemHolderLi = document.createElement("li");
+      let divItem = document.createElement("div");
+      divItem.classList.add("food__menu--item");
+      divItem.classList.add("collapsible-header");
+
+      //append to divItem
+      let divName = document.createElement("div");
+      divName.classList.add("food__name");
+      divName.textContent = item.name;
+
+      let divPrice = document.createElement("div");
+      divPrice.classList.add("food__price");
+      divPrice.textContent = formatter.format(item.price);
+
+      let divContent = document.createElement("div");
+      divContent.classList.add("collapsible-body");
+
+      divContent.textContent =
+        "LKslda orem ipsum dolor sit amet orem ipsum dolor sit ametorem ipsum dolor sit amet ";
+
+
+      divItem.appendChild(divName);
+      divItem.appendChild(divPrice);
+      itemHolderLi.appendChild(divItem);
+      itemHolderLi.appendChild(divContent);
+      itemHolderUl.appendChild(itemHolderLi);
+
+    });
+    divCateD.appendChild(itemHolderUl);
+    divType.appendChild(divCateD);
+
+  });
+
+  foodMenu.appendChild(divType);
+
+
+
+
+}
+// loadMenu = () => {
+//   let arrTS = menuItemsJSON["tibetan specialties"];
+//   let arrAS = menuItemsJSON["Asian snacks"];
+//   let arrD = menuItemsJSON["Drinks"];
+//   let arrDessert = menuItemsJSON.Dessert;
+
+//   // TODO: Write less code than below solutions
+
+//   //----------------------------------------------Tibetan Specialties
+
+//   //appen to let foodMenu
+//   let divTypeTS = document.createElement("div");
+//   divTypeTS.classList.add("food__menu--type");
+
+//   divTypeTS.textContent = "Tibetan Specialties";
+//   let itemHolderUl = document.createElement("ul");
+//   itemHolderUl.classList.add("collapsible");
+//   arrTS.forEach(item => {
+//     //append to divType
+
+//     let itemHolderLi = document.createElement("li");
+//     let divItem = document.createElement("div");
+
+//     divItem.classList.add("food__menu--item");
+//     divItem.classList.add("collapsible-header");
+//     //append to divItem
+//     let divName = document.createElement("div");
+//     divName.classList.add("food__name");
+
+//     divName.textContent = item.name;
+
+//     let divPrice = document.createElement("div");
+//     divPrice.classList.add("food__price");
+//     divPrice.textContent = formatter.format(item.price);
+
+//     let divContent = document.createElement("div");
+//     divContent.classList.add("collapsible-body");
+
+//     divContent.textContent =
+//       "LKslda orem ipsum dolor sit amet orem ipsum dolor sit ametorem ipsum dolor sit amet ";
+
+//     divItem.appendChild(divName);
+//     divItem.appendChild(divPrice);
+
+//     itemHolderLi.appendChild(divItem);
+//     itemHolderLi.appendChild(divContent);
+
+//     itemHolderUl.appendChild(itemHolderLi);
+//   });
+//   divTypeTS.appendChild(itemHolderUl);
+//   //----------------------------------------------------------------------
+//   let divTypeAS = document.createElement("div");
+//   divTypeAS.classList.add("food__menu--type");
+//   divTypeAS.textContent = "Asian Snacks";
+
+//   arrAS.forEach(item => {
+//     //append to divType
+//     let divItem = document.createElement("div");
+//     divItem.classList.add("food__menu--item");
+
+//     //append to divItem
+//     let divName = document.createElement("div");
+//     divName.classList.add("food__name");
+//     divName.textContent = item.name;
+
+//     let divPrice = document.createElement("div");
+//     divPrice.classList.add("food__price");
+//     divPrice.textContent = formatter.format(item.price);
+
+//     divItem.appendChild(divName);
+//     divItem.appendChild(divPrice);
+//     divTypeAS.appendChild(divItem);
+//   });
+
+//   // ------------------------------------------------ Drinks
+//   let divTypeD = document.createElement("div");
+//   divTypeD.classList.add("food__menu--type");
+//   divTypeD.textContent = "Drinks";
+
+//   arrD.forEach(category => {
+//     let divCateD = document.createElement("div");
+//     divCateD.classList.add("food__menu--category");
+//     divCateD.textContent = Object.keys(category)[0];
+//     divTypeD.appendChild(divCateD);
+//     category[Object.keys(category)[0]].forEach(item => {
+//       //append to divType
+//       let divItem = document.createElement("div");
+//       divItem.classList.add("food__menu--item");
+
+//       //append to divItem
+//       let divName = document.createElement("div");
+//       divName.classList.add("food__name");
+//       divName.textContent = item.name;
+
+//       let divPrice = document.createElement("div");
+//       divPrice.classList.add("food__price");
+//       divPrice.textContent = formatter.format(item.price);
+
+//       divItem.appendChild(divName);
+//       divItem.appendChild(divPrice);
+//       divTypeD.appendChild(divItem);
+//     });
+//   });
+
+//   // ------------------------------------------------ Dessert
+//   let divTypeDessert = document.createElement("div");
+//   divTypeDessert.classList.add("food__menu--type");
+//   divTypeDessert.textContent = "Dessert";
+
+//   arrDessert.forEach(item => {
+//     //append to divType
+//     let divItem = document.createElement("div");
+//     divItem.classList.add("food__menu--item");
+
+//     //append to divItem
+//     let divName = document.createElement("div");
+//     divName.classList.add("food__name");
+//     divName.textContent = item.name;
+
+//     let divPrice = document.createElement("div");
+//     divPrice.classList.add("food__price");
+//     divPrice.textContent = formatter.format(item.price);
+
+//     divItem.appendChild(divName);
+//     divItem.appendChild(divPrice);
+//     divTypeDessert.appendChild(divItem);
+//   });
+
+//   foodMenu.appendChild(divTypeTS);
+//   foodMenu.appendChild(divTypeAS);
+//   foodMenu.appendChild(divTypeD);
+//   foodMenu.appendChild(divTypeDessert);
+// };
+
+
+loadMenuSmall();
+//loadMenu();
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, {
+    inDuration: 500,
+    outDuration: 300
+  });
+});
---
+++ js/menu_items.js
@@ -0,0 +1,51 @@
+let menuItemsJSON = {
+  "tibetan specialties": [
+    { name: "Steamed Momo’s (8 pcs)", price: "5" },
+    { name: "Fried Momo’s (8 pcs)", price: "7" },
+    { name: "Chowmein", price: "6" },
+    { name: "Shabaley (2 pc)", price: "6" },
+    { name: "Fried rice", price: "6" },
+    { name: "Thukpa", price: "6" },
+    { name: "Shapta with 2 tingmo’s", price: "8" },
+    { name: "Shapta only", price: "6" },
+    { name: "Chilli chicken with 2 tingmo’s", price: "8" },
+    { name: "Chilly chicken only", price: "6" },
+    { name: "Tingmo", price: "1" },
+    { name: "Rice", price: "2" }
+  ],
+  "Asian snacks": [{ name: "Lumpia(3 pcs)", price: "5" }],
+
+  Drinks: [
+    {
+      "warm drinks": [
+        { name: "Tibetan butter tea	", price: "2.5" },
+        { name: "Indian masala Chai", price: "2.5" },
+        { name: "Tea", price: "2" },
+        { name: "Coffee", price: "2" }
+      ]
+    },
+    {
+      "cold drinks": [
+        { name: "Cola, Cola Zero", price: "2.2" },
+        { name: "Fanta", price: "2.2" },
+        { name: "Ice Tea", price: "2.2" },
+        { name: "Fresh Orange Juice", price: "2.2" }
+      ]
+    },
+    {
+      "Alcoholic drinks": [
+        { name: "Brugse Zot", price: "3" },
+        { name: "Leffe blond/brown", price: "3" },
+        { name: "Jupiler", price: "3" }
+      ]
+    },
+    {
+      Wine: [
+        { name: "Red/white/Rosé (glas)", price: "4" },
+        { name: "Champaign (glas)", price: "4" }
+      ]
+    }
+  ],
+
+  Dessert: [{ name: "Damblanch", price: "5" }, { name: "Sorbé", price: "5" }]
+};
